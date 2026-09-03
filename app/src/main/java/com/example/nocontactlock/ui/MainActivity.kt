d32580d9bf579c496ddac1bd9885835916e1bef6
lurdny3496-source <lurdny3496@gmail.com>
2026-09-02 21:41:54 -0400

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/nocontactlock/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nocontactlock.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+    }
+}
